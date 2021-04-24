325aa24b1bd1c2a54fde3dde718e7a9d4b2e47a3
Carolina Guida <carolinaguidaa@gmail.com>
2021-04-24 15:26:52 -0300

    feat: Add router and Bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div class="container">
+      <header>Navbar</header>
+    <div class="container-body">
+      <main>
+        <router-view></router-view>
+      </main>
+    </div>
   </div>
-</template>
+ </template>
+
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  
+export default{
+  name: 'App'  
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <b-form-input
+          v-model="text"
+          placeholder="Insira o Pokemon desejado"
+        ></b-form-input>
+        <div class="mt-2">Value: {{ text }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      text: "",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
